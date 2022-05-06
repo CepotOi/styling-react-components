cf740352fa9b1f9473d5ca5230dcb5e922f5391c
CepotOi <rizkihutama69@gmail.com>
2022-05-06 16:44:34 +0700

    setting css classes dynamically

--- src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -5,13 +5,26 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isInputValid, setInputValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setInputValid(true);
+    } else {
+      setInputValid(false);
+    }
+
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setInputValid(false);
+      return;
+    };
+
+    setInputValid(true);
     props.onAddGoal(enteredValue);
 
     return setEnteredValue('');
@@ -19,9 +32,11 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={!isInputValid ? 'form-control invalid' : 'form-control'}>
         <label>Course Goal</label>
-        <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
+        <input type="text"
+          value={enteredValue} onChange={goalInputChangeHandler} placeholder={!isInputValid ? 'Please fill this input' : ''
+          } />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
